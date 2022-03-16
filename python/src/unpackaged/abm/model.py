2dd797159ce3dbf8f7ea01ed2f91291c5e37f34d
Sebastian Jones <sebastian.jones@outlook.com>
2022-03-16 21:59:47 +0000

    Update model.py

--- python/src/unpackaged/abm/model.py
+++ python/src/unpackaged/abm/model.py
@@ -5,73 +5,38 @@ import operator
 import matplotlib
 import matplotlib.pyplot
 
+# control number of agents
+num_of_agents = 10
+num_of_iterations = 100
+
 # make agent list
 
 agents = []
 
 # make random integers and append to agents list
 
-agents.append([random.randint(0,99),random.randint(0,99)])
+for i in range(num_of_agents):
+    agents.append([random.randint(0,99),random.randint(0,99)])
 
-# move y0 randomly
+for i in range(num_of_agents):
 
-if random.random() < 0.5:
-    agents[0][0] += 1
-else:
-    agents[0][0] -= 1
+    #move agents randomly
 
-# move x0 randomly
+    for j in range(num_of_iterations):
 
-if random.random() < 0.5:
-    agents[0][1] += 1
-else:
-    agents[0][1] -= 1
+        # move y0 randomly
 
-# move y0 randomly again
+        if random.random() < 0.5:
+            agents[i][0] = (agents[i][0] + 1) % 100
+        else:
+            agents[i][0] = (agents[i][0] - 1) % 100
 
-if random.random() < 0.5:
-    agents[0][0] += 1
-else:
-    agents[0][0] -= 1
+        # move x0 randomly
 
-# move x0 randomly again
-
-if random.random() < 0.5:
-    agents[0][1] += 1
-else:
-    agents[0][1] -= 1
-
-# make random integers and append to agents list
-
-agents.append([random.randint(0,99),random.randint(0,99)])
-
-# move y1 randomly
-
-if random.random() < 0.5:
-    agents[1][0] += 1
-else:
-    agents[1][0] -= 1
-
-# move x1 randomly
-
-if random.random() < 0.5:
-    agents[1][1] += 1
-else:
-    agents[1][1] -= 1
-
-# move y1 randomly again
-
-if random.random() < 0.5:
-    agents[1][0] += 1
-else:
-    agents[1][0] -= 1
-
-# move x1 randomly again
-
-if random.random() < 0.5:
-    agents[1][1] += 1
-else:
-    agents[1][1] -= 1
+        if random.random() < 0.5:
+            agents[i][1] = (agents[i][1] + 1) % 100
+        else:
+            agents[i][1] = (agents[i][1] - 1) % 100
 
 # print agents]
 
@@ -79,22 +44,22 @@ print(agents)
 
 # calculate distance between points
 
-y_diff = (agents[0][0] - agents[1][0])
-y_diffsq = y_diff * y_diff
+# y_diff = (agents[0][0] - agents[1][0])
+# y_diffsq = y_diff * y_diff
 
-x_diff = (agents[0][1] - agents[1][1])
-x_diffsq = x_diff * x_diff
+# x_diff = (agents[0][1] - agents[1][1])
+# x_diffsq = x_diff * x_diff
 
-sum = y_diffsq + x_diffsq
+# sum = y_diffsq + x_diffsq
 
-answer = sum**0.5
+# answer = sum**0.5
 
-print(answer)
+# print(answer)
 
-print(max(agents, key=operator.itemgetter(1)))
+# print(max(agents, key=operator.itemgetter(1)))
 
 matplotlib.pyplot.ylim(0, 99)
 matplotlib.pyplot.xlim(0, 99)
-matplotlib.pyplot.scatter(agents[0][1],agents[0][0], color='red')
-matplotlib.pyplot.scatter(agents[1][1],agents[1][0], color='red')
+for i in range(num_of_agents):
+    matplotlib.pyplot.scatter(agents[i][1],agents[i][0])
 matplotlib.pyplot.show()
